678b3591bac011e4e5abf39821d4a3cd052442ef
Emigrate <tvemigrate@gmail.com>
2021-09-10 19:14:27 +0300

    fix:css html js

--- task3/app.js
+++ task3/app.js
@@ -1,25 +1,33 @@
-const linkDark = document.querySelector(".button-dark")
+const linkDark = document.querySelector("#button-dark")
 const linkBody = document.querySelector("body")
-const linkLogin = document.querySelector(".button-login")
+const linkLogin = document.querySelector("#button-login")
 const linkPopup = document.querySelector("#popuptoggle")
-const cancelbtn = document.querySelector(".cancelbtn")
+const cancelbtn = document.querySelector(".cancel-btn")
 const bcontainer = document.querySelector(".b-container")
+const blackContainer = document.querySelector("#blackcontainer")
 
 linkDark.addEventListener("click", function () {
     linkBody.classList.toggle("dark");
 })
 
 linkLogin.addEventListener("click", function() {
+    blackContainer.classList.remove("b-popup");
+    blackContainer.classList.add("b-container");
     linkPopup.classList.remove("b-popup");
     linkPopup.classList.add("b-popup-show");
 })
 
 cancelbtn.addEventListener("click", function() {
-    linkPopup.classList.remove("b-popup.show");
+    linkPopup.classList.remove("b-popup-show");
     linkPopup.classList.add("b-popup");
+    blackContainer.classList.add("b-popup");
+    blackContainer.classList.remove("b-container");
 })
 
-bcontainer.addEventListener("click", function() {
-    linkPopup.classList.remove("b-popup.show");
+blackContainer.addEventListener("click", function() {
+    linkPopup.classList.remove("b-popup-show");
     linkPopup.classList.add("b-popup");
+    blackContainer.classList.add("b-popup");
+    blackContainer.classList.remove("b-container");
 })
+
